198f96a3ae7d5e1c3aed8e96935e0bbda80f821c
Hikmet Can <hiko.kufte@gmail.com>
2022-09-08 01:03:31 +0300

    feat! text section added

--- src/components/sections/Text.vue
+++ src/components/sections/Text.vue
@@ -2,13 +2,14 @@
 import { useMainStore } from "@/stores/main";
 import { storeToRefs } from "pinia";
 
-const props = defineProps(["sectionData", "sectionIndex"]);
+const props = defineProps(["sectionIndex"]);
 const store = useMainStore();
-// const { sections } = storeToRefs(store);
+const { sections } = storeToRefs(store);
+const { deleteItem } = store;
 </script>
 <template>
-    <div>
-        {{ sectionData }}
-        {{ sectionIndex }}
+    <div class="flex flex-row items-center">
+        <textarea v-model="sections[sectionIndex].data[0]" class="w-full border border-transparent pl-1 rounded outline-none hover:border-gray-400 focus:border-gray-800"> </textarea>
+        <button v-if="sections[sectionIndex].data[0] !== undefined" @click="deleteItem(sectionIndex, 0)" class="ml-2 w-5 h-5">🗑️</button>
     </div>
 </template>
