8128654c8b1cb896c44c31bda5630e371541bdf5
Hikmet Can <hiko.kufte@gmail.com>
2022-09-11 00:08:29 +0300

    feat! font awesome added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import App from "./App.vue";
 
 import "./index.css";
 
-const app = createApp(App);
+/*  Font Awesome    */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPhone, faEnvelope, faLocationDot, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, faFacebook, faInstagram, faLinkedin, faGithub, faDiscord, faYoutube } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faPhone, faEnvelope, faLocationDot, faHouse, faTwitter, faFacebook, faInstagram, faLinkedin, faGithub, faDiscord, faYoutube);
+/*  Font Awesome    */
+
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 
